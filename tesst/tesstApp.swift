621b2990093558c388c9f777805af8be0dd4b7b6
Endrit <endritg60@gmail.com>
2023-08-12 06:56:02 -0700

    Initial Commit

---
+++ tesst/tesstApp.swift
@@ -0,0 +1,17 @@
+//
+//  tesstApp.swift
+//  tesst
+//
+//  Created by user933037 on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct tesstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
